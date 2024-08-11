d08caafc9456d9e8d11355166c9ba0cffaed3332
Dispy-Mac <tpps89305@hotmail.com>
2024-08-11 20:35:00 +0800

    Fixed issue of iOS app cannot play ogg files

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,23 +6,23 @@ class OverlayName {
 
 class AssertName {
   static const List<String> ouchAudioNames = [
-    'sfx/ouch1.ogg',
-    'sfx/ouch2.ogg',
-    'sfx/ouch3.ogg',
-    'sfx/ouch4.ogg',
-    'sfx/ouch5.ogg',
-    'sfx/ouch6.ogg',
-    'sfx/ouch7.ogg',
-    'sfx/ouch8.ogg',
-    'sfx/ouch9.ogg',
-    'sfx/ouch10.ogg',
-    'sfx/ouch11.ogg',
+    'sfx/ouch1.mp3',
+    'sfx/ouch2.mp3',
+    'sfx/ouch3.mp3',
+    'sfx/ouch4.mp3',
+    'sfx/ouch5.mp3',
+    'sfx/ouch6.mp3',
+    'sfx/ouch7.mp3',
+    'sfx/ouch8.mp3',
+    'sfx/ouch9.mp3',
+    'sfx/ouch10.mp3',
+    'sfx/ouch11.mp3',
   ];
   static const List<String> hahaAudioNames = [
-    'sfx/haha1.ogg',
-    'sfx/haha2.ogg',
-    'sfx/haha3.ogg',
-    'sfx/haha4.ogg',
-    'sfx/haha5.ogg',
+    'sfx/haha1.mp3',
+    'sfx/haha2.mp3',
+    'sfx/haha3.mp3',
+    'sfx/haha4.mp3',
+    'sfx/haha5.mp3',
   ];
 }
